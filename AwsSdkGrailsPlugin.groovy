b5fd7a3520345790eb82e107bffa459f39b0b1b7
Benoit Hediard <hediard@affinitiz.com>
2012-09-27 12:50:58 +0200

    Version bump to 1.3.21

--- AwsSdkGrailsPlugin.groovy
+++ AwsSdkGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class AwsSdkGrailsPlugin {
 
-    def version = "1.3.12"
+    def version = "1.3.21"
     def grailsVersion = "2.0 > *"
     def dependsOn = [:]
     def loadAfter = ['services', 'controllers']
