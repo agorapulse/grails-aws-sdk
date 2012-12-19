cdece79e8f856b45b98d2904ee80723c8f645f0d
Benoit Hediard <hediard@affinitiz.com>
2012-12-19 15:11:25 +0100

    AWS Java SDK dependency upgraded to 1.3.26

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,7 +10,12 @@ grails.project.dependency.resolution = {
         mavenCentral()
     }
     dependencies {
-        compile 'com.amazonaws:aws-java-sdk:1.3.24'
+        // Workaround to resolve dependency issue with aws-java-sdk and http-builder (dependent on httpcore:4.0)
+        build 'org.apache.httpcomponents:httpcore:4.1'
+        build 'org.apache.httpcomponents:httpclient:4.1'
+        runtime 'org.apache.httpcomponents:httpcore:4.1'
+        runtime 'org.apache.httpcomponents:httpclient:4.1'
+        compile 'com.amazonaws:aws-java-sdk:1.3.26'
     }
     plugins {
         build(':release:2.2.0', ':rest-client-builder:1.0.3') {
