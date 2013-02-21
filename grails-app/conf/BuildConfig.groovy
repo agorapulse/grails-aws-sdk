0838f0bebaad7276f7c9093a8b5f3bda729c0e8b
Benoit Hediard <ben@agorapulse.com>
2013-02-21 10:41:26 +0100

    Release plugin upgrade to 2.2.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -18,7 +18,7 @@ grails.project.dependency.resolution = {
         compile 'com.amazonaws:aws-java-sdk:1.3.32'
     }
     plugins {
-        build(':release:2.2.0', ':rest-client-builder:1.0.3') {
+        build(':release:2.2.1', ':rest-client-builder:1.0.3') {
             export = false
         }
     }
