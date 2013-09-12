3ce8249995bab48ad91d2c1ec8e01c4ae1e96222
Benoit Hediard <ben@agorapulse.com>
2013-09-12 10:12:00 +0200

    BuildConfig updated to use maven dependency resolver

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,9 +1,22 @@
-grails.project.work.dir = 'target'
+grails.servlet.version = '3.0' // Change depending on target container compliance (2.5 or 3.0)
+grails.project.class.dir = 'target/classes'
+grails.project.test.class.dir = 'target/test-classes'
+grails.project.test.reports.dir = 'target/test-reports'
+grails.project.work.dir = 'target/work'
+grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 
+grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
-    inherits 'global'
-    log 'warn'
+    // inherit Grails' default dependencies
+    inherits('global') {
+        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
+        // excludes 'ehcache'
+    }
+    log 'error' // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true // Whether to verify checksums on resolve
+    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+
     repositories {
         grailsCentral()
         mavenLocal()
