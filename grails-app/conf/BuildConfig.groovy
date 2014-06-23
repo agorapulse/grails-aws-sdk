cb0c27a12db35ba6a86891331dc1703fd888ba24
Benoit Hediard <ben@agorapulse.com>
2014-06-23 09:32:06 +0200

    AWS SDK java lib upgraded to 1.8.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,15 +29,12 @@ grails.project.dependency.resolution = {
         build 'org.apache.httpcomponents:httpmime:4.3.3'
         runtime 'org.apache.httpcomponents:httpcore:4.2'
         runtime 'org.apache.httpcomponents:httpclient:4.2'
-        compile 'com.amazonaws:aws-java-sdk:1.7.11'
+        compile 'com.amazonaws:aws-java-sdk:1.8.0'
     }
     plugins {
         build(':release:3.0.1',
                 ':rest-client-builder:1.0.3',
                 ':coveralls:0.1') {
-            export = false
-        }
-        build() {
             export = false
         }
         test(':code-coverage:1.2.7') {
